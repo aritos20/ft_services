7f58e661386fcb51c5f2551ef27cad5827a8724d
Aristidis Gianicopulos loguercio <agianico@c1r14s1.42madrid.com>
2021-06-07 16:41:47 +0200

    Project approved in 42

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
 eval $(minikube docker-env)
 kubectl delete -f srcs/$1.yaml
-docker rmi $1      
-docker build -t $1 srcs/$1    
+docker rmi $1
+docker build -t $1 srcs/$1
 kubectl apply -f srcs/$1.yaml
